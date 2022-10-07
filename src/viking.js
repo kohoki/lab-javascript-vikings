ebf2db1c7c0a7347c96a0c985dbaef3613d4b10d
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-07 15:09:19 +0200

    Finish Bonus1 to 90%

--- src/viking.js
+++ src/viking.js
@@ -45,4 +45,51 @@ class Saxon extends Soldier{
     }
   }
 // War
-class War {}
+class War {
+    constructor(){
+      this.vikingArmy = [];
+      this.saxonArmy = [];
+    }
+    addViking(Viking){
+        this.vikingArmy.push(Viking);
+    }
+  
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon);
+    }
+  
+    vikingAttack(){
+        let randomSaxon = this.saxonArmy[ Math.floor(Math.random() * (this.saxonArmy.length))];
+        let randomViking = this.vikingArmy[ Math.floor(Math.random() * (this.vikingArmy.length))];
+    
+        //console.log(randomSaxon.health);
+        let result = randomSaxon.receiveDamage(randomViking.strength);
+        if (randomSaxon.health < 0)
+        {
+          let index = this.saxonArmy.indexOf(randomSaxon);
+          this.saxonArmy.splice(index,1);
+          
+        }
+        return result;
+        //console.log(this.saxonArmy);
+      }
+  
+    saxonAttack(){
+        let randomSaxon = this.saxonArmy[ Math.floor(Math.random() * (this.saxonArmy.length))];
+        let randomViking = this.vikingArmy[ Math.floor(Math.random() * (this.vikingArmy.length))];
+      
+        let result = randomViking.receiveDamage(randomSaxon.strength);
+        if (randomViking.health < 0)
+        {
+          let index = this.vikingArmy.indexOf(randomViking);
+          this.vikingArmy.splice(index,1);
+        }
+        return result;
+      }
+    
+  
+    showStatus(){
+  
+    }
+  }
+  
